a5a406d2ef78c71fa558246b17d3f45feec65d28
Anton <m-a-r-v-i-n@mail.ru>
2018-11-26 16:31:22 +0300

    Добавил 4 контейнера для ограничения видимости
    Добавил 4 контейнера: ShowUser, ShowGuest, ShowSelf, ShowExceptSelf

---
+++ src/containers/ShowExceptSelf.jsx
@@ -0,0 +1,30 @@
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { setRedirectPath } from "actions/actionsAuth";
+
+class ShowExceptSelf extends PureComponent {
+  static propTypes = {
+    isAuthenticated: PropTypes.bool,
+    aid: PropTypes.number,
+    uid: PropTypes.number,
+    children: PropTypes.any
+  };
+
+  render() {
+    let { children, isAuthenticated, aid, uid } = this.props;
+
+    return isAuthenticated && aid && uid && aid !== uid ? children : "";
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    ...ownProps,
+    isAuthenticated: state.auth.isAuthenticated,
+    aid: state.auth.userId
+  };
+};
+
+export default connect(mapStateToProps)(ShowExceptSelf);
